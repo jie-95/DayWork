5e92866f7352faee1f20a109086b78c82bd7e5ac
yang <young_yang95@163.com>
2022-08-14 21:46:59 +0800

    8.14作业

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <HelloWorld :total="total"/>
+  </div>
+</template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  // eslint-disable-next-line
+  data() {
+    return {
+      total: 80
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
